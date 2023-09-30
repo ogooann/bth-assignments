9852b5c5258b9698b8d323934c99577673c2ed88
ogooann <144517508+ogooann@users.noreply.github.com>
2023-09-30 13:39:13 +0200

    Add files via upload

---
+++ clientmain.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+/* You will to add includes here */
+
+
+// Included to get the support library
+#include <calcLib.h>
+
+
+#include "protocol.h"
+
+int main(int argc, char *argv[]){
+  
+  /* Do magic */
+
+}
